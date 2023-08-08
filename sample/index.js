16378113d2fe4fd6628bb0ee0c21811c766c0937
expiteRz <none@rz-public.xyz>
2023-08-08 11:17:25 +0900

    Initial commit

---
+++ sample/index.js
@@ -0,0 +1,35 @@
+let socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/ws");
+
+let roomId = document.getElementById("room_id");
+let trackName = document.getElementById("track");
+let memberOutput = document.getElementById("members");
+
+socket.onopen = () => {
+    console.log("Connected");
+}
+
+socket.onclose = e => {
+    console.log("Connection closed: ", e);
+    socket.send("Client closed");
+}
+
+socket.onerror = err => {
+    console.log("Error occurred: ", err);
+}
+
+socket.onmessage = e => {
+    let data = JSON.parse(e.data);
+
+    if (data.status !== "success") {
+        roomId.innerText = "Offline!"
+        trackName.innerText = ""
+        memberOutput.innerHTML = "" // Initialize element
+        return;
+    }
+    roomId.innerText = data.id;
+    trackName.innerText = data.setting.course;
+    data.members.forEach(v => {
+        memberOutput.innerHTML += `<li>${v.name}</li>`
+        if (v.guest_name) memberOutput.innerHTML += `<li>${v.guest_name}</li>`
+    })
+}
